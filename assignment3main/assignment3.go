997da705e7adff77e0e8aff901a073267b190b79
Salman Saleh <salmansaleh786@gmail.com>
2019-04-04 23:34:09 +0500

    editing

--- assignment3main/assignment3.go
+++ assignment3main/assignment3.go
@@ -2,12 +2,12 @@ package main
 
 import (
 	"fmt"
-	"github.com/SalmanSaleh786/golangexamples"
+	"github.com/SalmanSaleh786/golang/golangexamples"
 )
 
 func main() {
 	b := []byte{'g', 'o', 'l', 'a', 'n', 'g'}
 	println(golangexamples.ConcatSlice(b))
-	fmt.Printf("%d", golangexamples.Encrypt(b, 3))
+	fmt.Printf("%s", golangexamples.Encrypt(b, 3))
 	fmt.Printf("%s", golangexamples.EZGreetings("This is salman"))
 }
